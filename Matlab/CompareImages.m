10b33603ad0df79a401d793617b40b069da44ac6
Wouter Groen <wa.groen@gmail.com>
2020-03-03 18:04:25 +0100

    now with actual matlab files

---
+++ Matlab/CompareImages.m
@@ -0,0 +1,83 @@
+function stat = CompareImages(gt, img, maxvalue)
+
+if ( nargin < 3 )
+    maxvalue = 255.0;
+end
+
+diffsquared = (gt - img).^2;
+
+% sum of squared differences
+stat.ssd     = sum(  sum( diffsquared ) );
+stat.ssdsum  = sum(  stat.ssd );
+% display( sprintf('SSD for first  channel  is %15.9f', stat.ssd(1) ) );
+% if (length(stat.ssd) > 1)
+% display( sprintf('SSD for second channel  is %15.9f', stat.ssd(2) ) );
+% end
+% if (length(stat.ssd) > 2)
+% display( sprintf('SSD for third  channel  is %15.9f', stat.ssd(3) ) );
+% end
+% display( sprintf('SSD for all    channels is %15.9f\n', stat.ssdsum ) );
+
+
+% mean squared error
+stat.mse     = mean( mean( diffsquared ) );
+stat.msesum  = sum(  stat.mse );
+% display( sprintf('MSE for first  channel  is %15.9f', stat.mse(1) ) );
+% if (length(stat.mse) > 1)
+% display( sprintf('MSE for second channel  is %15.9f', stat.mse(2) ) );
+% end
+% if (length(stat.mse) > 2)
+% display( sprintf('MSE for third  channel  is %15.9f', stat.mse(3) ) );
+% end
+% display( sprintf('MSE for all    channels is %15.9f\n', stat.msesum ) );
+
+
+% root mean squared error
+stat.rmse     = sqrt( stat.mse );
+stat.rmsesum  = sqrt( stat.msesum );
+% display( sprintf('RMSE for first  channel  is %15.9f', stat.rmse(1) ) );
+% if (length(stat.rmse) > 1)
+% display( sprintf('RMSE for second channel  is %15.9f', stat.rmse(2) ) );
+% end
+% if (length(stat.rmse) > 2)
+% display( sprintf('RMSE for third  channel  is %15.9f', stat.rmse(3) ) );
+% end
+%display( sprintf('RMSE for all    channels is %15.9f\n', stat.rmsesum ) );
+
+
+% peak signal-to-noise-ratio
+stat.psnr     = 20 * log10( maxvalue / stat.rmse );
+stat.psnrsum  = 20 * log10( maxvalue / stat.rmsesum );
+% display( sprintf('PSNR for first  channel  is %15.9f', stat.psnr(1) ) );
+% if (length(stat.psnr) > 1)
+% display( sprintf('PSNR for second channel  is %15.9f', stat.psnr(2) ) );
+% end
+% if (length(stat.psnr) > 2)
+% display( sprintf('PSNR for third  channel  is %15.9f', stat.psnr(3) ) );
+% end
+% display( sprintf('PSNR for all    channels is %15.9f\n', stat.psnrsum ) );
+
+
+% Structural SIMilarity (SSIM)
+[stat.ssim(1),stat.ssim_index(1,:,:)] = ssim( gt(:,:,1), img(:,:,1));%, [0.01 0.03], fspecial('gaussian', 11, 1.5), 1.0);
+if (length(stat.ssim) > 1)
+[stat.ssim(2),stat.ssim_index(2,:,:)] = ssim( gt(:,:,2), img(:,:,2));%, [0.01 0.03], fspecial('gaussian', 11, 1.5), 1.0);
+end
+if (length(stat.ssim) > 2)
+[stat.ssim(3),stat.ssim_index(3,:,:)] = ssim( gt(:,:,3), img(:,:,3));%, [0.01 0.03], fspecial('gaussian', 11, 1.5), 1.0);
+end
+stat.ssimsum  = sum(  stat.ssim );
+stat.ssimmean = mean( stat.ssim );
+% display( sprintf('SSIM        for first  channel  is %15.9f', stat.ssim(1) ) );
+% if (length(stat.ssim) > 1)
+% display( sprintf('SSIM        for second channel  is %15.9f', stat.ssim(2) ) );
+% end
+% if (length(stat.ssim) > 2)
+% display( sprintf('SSIM        for third  channel  is %15.9f', stat.ssim(3) ) );
+% end
+% display( sprintf('Sum of SSIM for all    channels is %15.9f', stat.ssimsum ) );
+% display( sprintf('Mean SSIM   for all    channels is %15.9f\n', stat.ssimmean ) );
+
+% end function
+
+
---
+++ Matlab/ConvergenceGraph.m
@@ -0,0 +1,122 @@
+function ConvergenceGraph(name, graph_name)
+
+BaseDirectory           = 'C:\Users\nomen\Documents\Thesis\tests\';
+Directory               = strcat(BaseDirectory, strcat(name, '\'));
+Techniques              = {'PhaseFunctionOnly', 'Hybrid', 'LightPaths', 'LightPathsOcto', 'LightPathsOctoGradient'}; %folder name
+ID                      = 1;
+TechniqueIDs            = [];
+Series                  = {'PhaseFunctionOnly', 'Hybrid', 'LightPaths', 'LightPathsOcto', 'LightPathsOctoGradient'}; %graph name
+SPP                     = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+MaxSPP                  = 9;
+Errors                  = zeros(1);
+Timings                 = zeros(1);
+ReferenceImageFile      = dir([Directory 'reference*.png']);
+ReferenceImage          = double(imread(strcat(Directory, ReferenceImageFile.name))) / 255.0;
+Figure                  = figure('Name', name);
+AxisY                   = axes('Parent', Figure, 'YScale', 'log', 'YMinorTick', 'on');
+Markers                 = { 'x', 'v', 'square', '^', '+' }     ;  
+Results                 = cell(1, 1);
+
+for i = 1 : length(Series)
+    Results(1, i + 1) = Series(i);
+end
+
+for i = 1 : length(SPP)
+    Results(i + 1, 1) = {SPP(i)};
+end
+
+box(AxisY, 'on');
+hold(AxisY, 'all');
+xlim(AxisY, [0 SPP(MaxSPP)]);
+
+for i = 1 : length(Techniques)
+    TechniqueDir = sprintf('%s%s\\', Directory, Techniques{i});
+    
+    if exist(TechniqueDir, 'dir')
+        TechniqueIDs(ID) = i;
+        CSV = readmatrix(sprintf('%smeasurements.csv', TechniqueDir));
+        
+        for j = 1 : length(SPP)           
+            ImageFileName = sprintf('%simages\\%d.png', TechniqueDir, SPP(j));
+
+            if exist(ImageFileName, 'file')
+                Image       = double(imread(ImageFileName)) / 255.0;
+                Statistics  = CompareImages(Image, ReferenceImage, 1.0);
+                Error       = Statistics.msesum;
+                
+                if j <= MaxSPP
+                    Errors(j, ID) = Error;
+                    Timings(j, ID) = CSV(j,2);
+                end
+                
+                Results(j + 1, ID + 1) = {Error};
+            else
+                warning('File does not exist: %s', ImageFileName);
+            end
+        end
+        
+        ID = ID + 1;
+    else
+        warning('Directory does not exist: %s', TechniqueDir);
+    end
+end
+
+Plot = plot(SPP(1:MaxSPP), Errors);
+
+for i = 1 : length(TechniqueIDs)
+    TechniqueID = TechniqueIDs(i);
+    set(Plot(i), 'DisplayName', Series{TechniqueID});
+    set(Plot(i), 'Marker', Markers{TechniqueID});
+end
+
+title(graph_name, 'FontSize', 12);
+xlabel('Samples per pixel', 'FontSize', 10);
+ylabel('Mean-Squared Error', 'FontSize', 10);
+legend(AxisY, 'show');
+
+ConvergencePdfFilePath = sprintf('%sconvergence.pdf', Directory);
+
+TI = get(gca, 'TightInset');
+
+set(gca, 'Position', [TI(1) TI(2) 1 - TI(3) - TI(1) 1 - TI(4) - TI(2)]);
+set(gca, 'units', 'centimeters')
+
+P   = get(gca,'Position');
+TI  = get(gca, 'TightInset');
+
+set(gcf, 'PaperUnits', 'centimeters');
+set(gcf, 'PaperSize', [P(3) + TI(1) + TI(3) P(4) + TI(2) + TI(4)]);
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperPosition', [0 0 P(3) + TI(1) + TI(3) P(4) + TI(2) + TI(4)]);
+
+saveas(Figure, ConvergencePdfFilePath, 'pdf');
+
+xlswrite(sprintf('%serrors.xlsx', Directory), Results);
+
+FigureTimings = figure('Name', append(name, '-Timings'));
+%box(AxisY, 'on');
+%hold(AxisY, 'all');
+%xlim(AxisY, [0 SPP(MaxSPP)]);
+TimingsPlot = plot(SPP(1:MaxSPP), Timings);
+for i = 1 : length(TechniqueIDs)
+    TechniqueID = TechniqueIDs(i);
+    set(TimingsPlot(i), 'DisplayName', Series{TechniqueID});
+    set(TimingsPlot(i), 'Marker', Markers{TechniqueID});
+end
+title(graph_name, 'FontSize', 12);
+xlabel('Samples per pixel', 'FontSize', 10);
+ylabel('Time in ms', 'FontSize', 10);
+legend('show', 'Location', 'northwest');
+TI = get(gca, 'TightInset');
+
+set(gca, 'Position', [TI(1) TI(2) 1 - TI(3) - TI(1) 1 - TI(4) - TI(2)]);
+set(gca, 'units', 'centimeters')
+
+P   = get(gca,'Position');
+TI  = get(gca, 'TightInset');
+
+set(gcf, 'PaperUnits', 'centimeters');
+set(gcf, 'PaperSize', [P(3) + TI(1) + TI(3) P(4) + TI(2) + TI(4)]);
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperPosition', [0 0 P(3) + TI(1) + TI(3) P(4) + TI(2) + TI(4)]);
+saveas(FigureTimings, sprintf('%stimings.pdf', Directory) , 'pdf');
